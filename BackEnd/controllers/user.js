25866617d6b0a6990c91a000752d36a5a65a49c3
MotazSoman <motaz.soman@gmail.com>
2022-09-11 22:12:55 +0300

    create function to register new user

---
+++ BackEnd/controllers/user.js
@@ -0,0 +1,40 @@
+// requer usermodel
+const usermodel = require("../models/UserSchema");
+
+// this function to creates a new  user
+const register = (req, res) => {
+  const { firstName, lastName, age, country, email, password } = req.body;
+  const user = new usermodel({
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    password,
+  });
+  user
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `account created successfully`,
+        result,
+      });
+    })
+    .catch((err) => {
+      // handel if the email is exists
+      if (err.keyPattern) {
+        return res
+          .status(409)
+          .json({ success: false, message: `the email already exists` });
+      }
+
+      res.status(500).json({
+        success: false,
+        message: `server error`,
+        err,
+      });
+      console.log(err);
+    });
+};
+module.exports = { register };
