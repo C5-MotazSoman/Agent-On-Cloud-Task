f2aee124b86507403de16b2d1a41e3f27405634d
MotazSoman <motaz.soman@gmail.com>
2022-09-12 00:27:21 +0300

    add authentication middleware and install jwt to verify the token

---
+++ BackEnd/middleware/authentication.js
@@ -0,0 +1,34 @@
+const jwt=require("jsonwebtoken")
+// function to check if logged in 
+
+const authentication=(req,res,next)=>{
+   try {
+    // check the token first is entered or not 
+    if (!req.headers.authorization) {
+        return res.status(403).json({
+            message:`forbidden`
+        })
+    }
+        // check the token if vaild or not 
+       const token=req.headers.authorization.split(" ").pop() 
+       jwt.verify(token,process.env.SECRET,(err,result)=>{
+        if (err) {
+            res.status(403).json({
+                success:false,
+                message:`the token is invalid or expired`
+            })
+            
+        }else{
+            req.token=result
+        }
+       })
+   } catch (error) {
+    res.status(500).json({
+        success:false,
+        message:`server error`,
+        error:error.message
+    })
+    
+   }
+}
+module.exports=authentication
