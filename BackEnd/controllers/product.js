e616370fb1f7e5ba151e3d3c850aa0ddb0c2ab5a
MotazSoman <motaz.soman@gmail.com>
2022-09-12 12:21:11 +0300

    create function to add product and function to get all product, and endpoint for them

--- BackEnd/controllers/product.js
+++ BackEnd/controllers/product.js
@@ -1,6 +1,58 @@
+const productModel = require("../models/productSchema");
+// function to creat product
+const addProduct = (req, res) => {
+  const { title, slug, desc, category, image, countInStock, price } = req.body;
+  const newProduct = new productModel({
+    title,
+    slug,
+    desc,
+    category,
+    image,
+    countInStock,
+    price,
+    userId: req.token.userId,
+  });
+  newProduct
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `product add successfully`,
+        result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `server error`,
+        err: err,
+      });
+    });
+};
 
-
-const getAllProducts=(req,res)=>{
-
-}
-module.exports={getAllProducts}
+// function to get all product
+const getAllProducts = (req, res) => {
+  const userId = req.token.userId;
+  productModel
+    .find({})
+    .then((result) => {
+      if (result.length) {
+        res.status(200).json({
+          success: true,
+          message: `all product`,
+          userId: userId,
+          result,
+        });
+      } else {
+        res.status(200).json({ success: false, message: `No product yet` });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: `server error`,
+        error,
+      });
+    });
+};
+module.exports = { getAllProducts, addProduct };
--- BackEnd/router/Product.js
+++ BackEnd/router/Product.js
@@ -3,8 +3,18 @@ const express=require("express")
 const authentication = require("../middleware/authentication")
 
 // import controller
-const {getAllProducts}=require("../controllers/product")
+const {getAllProducts, addProduct}=require("../controllers/product")
 
 const productRoter=express.Router()
 
+
+// endpoints 
 productRoter.get("/", authentication,getAllProducts)
+productRoter.post("/new",authentication,addProduct)
+
+
+
+
+
+
+module.exports=productRoter
