e392b0dfee01f1d647aa4a4c3b9efad479183c52
MotazSoman <motaz.soman@gmail.com>
2022-09-12 14:36:06 +0300

    add function to get single product with router , and delete authentication from getAllProducts

--- BackEnd/router/Product.js
+++ BackEnd/router/Product.js
@@ -3,20 +3,22 @@ const express=require("express")
 const authentication = require("../middleware/authentication")
 
 // import controller
-const {getAllProducts, addProduct,updatProductById,deleteProductById}=require("../controllers/product")
+const {getAllProducts, addProduct,updatProductById,deleteProductById,getProductById}=require("../controllers/product")
 
 const productRoter=express.Router()
 
 
 // endpoints :
 // endpoint to get all product 
-productRoter.get("/", authentication,getAllProducts)
+productRoter.get("/",getAllProducts)
 // end point to add product 
 productRoter.post("/new",authentication,addProduct)
 // endpoint to updat product
 productRoter.put("/:_id",authentication,updatProductById)
 // endpoint to delete product
 productRoter.delete("/delete/:_id",authentication,deleteProductById)
+// endpoint to get single product
+productRoter.get("/single/:_id",getProductById)
 
 
 
